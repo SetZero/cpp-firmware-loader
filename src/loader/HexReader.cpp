ed26e9cbeaffd5eab161758980fa191eb4a7ac3f
Unknown <19Sebastian95@gmx.de>
2019-06-05 01:16:47 +0200

    more verbose output

--- src/loader/HexReader.cpp
+++ src/loader/HexReader.cpp
@@ -7,13 +7,17 @@
 HexReader::HexReader(const std::string &fileLocation, const HexReader::byte &maxSize) {
     std::ifstream intelHexInput;
     intelHexInput.open(fileLocation, ifstream::in);
-    intelHexInput >> hex;
-    byte fileSize = HexReader::byte{static_cast<long>(hex.currentAddress())};
+	if (intelHexInput.good()) {
+		intelHexInput >> hex;
+		byte fileSize = HexReader::byte{ static_cast<long>(hex.currentAddress()) };
 
-    std::cout << "Final address is 0x" << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << hex.currentAddress() << std::dec << std::endl;
-    std::cout << "File size: " << fileSize << std::endl;
-    std::cout << "Max size: " << maxSize << std::endl;
-    if(fileSize < maxSize) {
-        std::cout << "Fit!" << std::endl;
-    }
+		std::cout << "Final address is 0x" << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << hex.currentAddress() << std::dec << std::endl;
+		std::cout << "File size: " << fileSize << std::endl;
+		std::cout << "Max size: " << maxSize << std::endl;
+		if (fileSize < maxSize) {
+			std::cout << "Fit!" << std::endl;
+		}
+	} else {
+		std::cout << "Failed to open: " << fileLocation << std::endl;
+	}
 }
