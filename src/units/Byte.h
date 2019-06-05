18e35e07166ad355bf5bf3af5764afcde132b449
Sebastian <19Sebastian95@gmx.de>
2019-06-05 14:48:43 +0200

    IEC prefix for Byte

--- src/units/Byte.h
+++ src/units/Byte.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include "../utils/RatioLookup.h"
 #include "SIUnit.h"
+#include "IECprefix.h"
 
 namespace CustomDataTypes::ComputerScience {
 
@@ -20,6 +21,7 @@ namespace CustomDataTypes::ComputerScience {
 		constexpr explicit Byte(Rep input) noexcept : parent_type{ input } {}
 	};
 
+	//SI-Prefix
 	using byte      = Byte<std::intmax_t>;
 	using kilobyte  = Byte<std::intmax_t, std::kilo>;
 	using megabyte  = Byte<std::intmax_t, std::mega>;
@@ -27,7 +29,15 @@ namespace CustomDataTypes::ComputerScience {
 	using terabyte  = Byte<std::intmax_t, std::tera>;
 	using petabyte  = Byte<std::intmax_t, std::peta>;
 
+	//IEC-Prefix
+    using kibibyte  = Byte<std::intmax_t, prefix::kibi>;
+    using mebibyte  = Byte<std::intmax_t, prefix::mebi>;
+    using gibibyte  = Byte<std::intmax_t, prefix::gibi>;
+    using tebibyte  = Byte<std::intmax_t, prefix::tebi>;
+    using pebibyte  = Byte<std::intmax_t, prefix::pebi>;
+
 	namespace literals {
+	    //SI-Prefix
 		constexpr byte operator ""_B(unsigned long long element) noexcept {
 			return byte(static_cast<std::intmax_t>(element));
 		}
@@ -67,6 +77,39 @@ namespace CustomDataTypes::ComputerScience {
 		constexpr Byte<long double, std::tera> operator ""_TB(long double element) noexcept {
 			return Byte<long double, std::tera>(element);
 		}
+
+		//IEC-Prefix
+        constexpr kilobyte operator ""_kiB(unsigned long long element) noexcept {
+            return kilobyte(static_cast<std::intmax_t>(element));
+        }
+
+        constexpr Byte<long double, prefix::kibi> operator ""_kiB(long double element) noexcept {
+            return Byte<long double, prefix::kibi>(element);
+        }
+
+        constexpr megabyte operator ""_MiB(unsigned long long element) noexcept {
+            return megabyte(static_cast<std::intmax_t>(element));
+        }
+
+        constexpr Byte<long double, prefix::mebi> operator ""_MiB(long double element) noexcept {
+            return Byte<long double, prefix::mebi>(element);
+        }
+
+        constexpr gigabyte operator ""_GiB(unsigned long long element) noexcept {
+            return gigabyte(static_cast<std::intmax_t>(element));
+        }
+
+        constexpr Byte<long double, prefix::gibi> operator ""_GiB(long double element) noexcept {
+            return Byte<long double, prefix::gibi>(element);
+        }
+
+        constexpr terabyte operator ""_TiB(unsigned long long element) noexcept {
+            return terabyte(static_cast<std::intmax_t>(element));
+        }
+
+        constexpr Byte<long double, prefix::tebi> operator ""_TiB(long double element) noexcept {
+            return Byte<long double, prefix::tebi>(element);
+        }
 	}
 }
 
