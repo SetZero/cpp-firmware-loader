19c8f0486658f9671148b2603df6ae453b18741f
Unknown <19Sebastian95@gmx.de>
2019-06-10 18:53:26 +0200

    unit parser test

--- src/units/parse/unitParser.h
+++ src/units/parse/unitParser.h
@@ -6,45 +6,52 @@
 #include <iostream>
 #include "../../utils/RatioLookup.h"
 #include "../..//utils/utils.h"
+#include "../..//utils/expected.h"
 
 namespace CustomDataTypes {
     //TODO: Add support for eg KiB
     template<typename T>
-    std::optional<T> parseUnit(const std::string& value) {
-        static std::regex unit_regex(
-            R"(([-+]?[0-9]*\.?[0-9]+)\s?(a|f|p|n|u|m|c|d|D|h|k|K|M|T|P|E)?([A-Z0-9a-z!"#$%&'()*+,.\/:;<=>?@\[\] ^_`{|}~-]+)?)",
-            std::regex_constants::ECMAScript);
-        std::smatch unit_match;
-        long double unit_val = 0.0;
-        std::pair<std::intmax_t, std::intmax_t> prefix;
-        std::optional<T> returnValue = std::nullopt;
-        std::string unit;
-        if (std::regex_search(value, unit_match, unit_regex)) {
-            for (size_t i = 1; i < unit_match.size(); ++i) {
-                switch (i) {
-                case 1:
-                    if (unit_match[i].length() > 0) {
-                        unit_val = Poco::NumberParser::parse(unit_match[i].str().data());
-                    }
-                    break;
-                case 2:
-                    if (unit_match[i].length() > 0) {
-                        prefix = utils::getRatio(unit_match[i].str().at(0));
-                    }
-                    break;
-                case 3:
-                    if (unit_match[i].length() > 0) {
-                        unit = unit_match[i].str();
-                        if (unit == utils::periodic_printable<T>::name) {
-                            using period_type = utils::periodic_info<T>::period;
-                            returnValue = T{ static_cast<utils::periodic_info<T>::rep>((unit_val * prefix.first * period_type::den) / (prefix.second * period_type::num)) };
+    utils::expected<T, const std::string> parseUnit(const std::string& value) {
+        utils::expected<T, const std::string> returnValue = utils::make_unexpected("There was an error specifying an Unit!");
+        try {
+            static std::regex unit_regex(
+                R"(([-+]?[0-9]*\.?[0-9]+)\s?(a|f|p|n|u|m|c|d|D|h|k|K|M|T|P|E)?([A-Z0-9a-z!"#$%&'()*+,.\/:;<=>?@\[\] ^_`{|}~-]+)?)",
+                std::regex_constants::ECMAScript);
+            std::smatch unit_match;
+            long double unit_val = 0.0;
+            std::pair<std::intmax_t, std::intmax_t> prefix;
+            std::string unit;
+            if (std::regex_search(value, unit_match, unit_regex)) {
+                for (size_t i = 1; i < unit_match.size(); ++i) {
+                    switch (i) {
+                    case 1:
+                        if (unit_match[i].length() > 0) {
+                            unit_val = Poco::NumberParser::parse(unit_match[i].str().data());
                         }
+                        break;
+                    case 2:
+                        if (unit_match[i].length() > 0) {
+                            prefix = utils::getRatio(unit_match[i].str().at(0));
+                        }
+                        break;
+                    case 3:
+                        if (unit_match[i].length() > 0) {
+                            unit = unit_match[i].str();
+                            if (unit == utils::periodic_printable<T>::name) {
+                                using period_type = utils::periodic_info<T>::period;
+                                returnValue = T{ static_cast<utils::periodic_info<T>::rep>((unit_val * prefix.first * period_type::den) / (prefix.second * period_type::num)) };
+                            }
+                        }
+                        break;
+                    default:
+                        break;
                     }
-                    break;
-                default:
-                    break;
                 }
             }
+        } catch (std::exception& e) {
+            std::stringstream ss;
+            ss << "There was an error while parsing an unit: \n" << e.what();
+            returnValue = utils::make_unexpected(ss.str());
         }
 
         return returnValue;
